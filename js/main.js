ced4038533367cd1fba0e026b84261d4e80cce19
이용만 <dydaks7878@gmail.com>
2023-08-27 17:01:34 +0900

    리워즈 적용

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ searchInputEl.addEventListener('blur', function(){
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function(){
-  console.log(window.scrollY);
   if(window.scrollY > 500){
     //배지 숨기기
     //gsap.to(요소, 지속시간, 옵션);
@@ -72,3 +71,16 @@ new Swiper('.promotion .swiper-container', {
   }
 });
 
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function(){
+  isHidePromotion = !isHidePromotion
+  if(isHidePromotion){
+    //숨김 처리
+    promotionEl.classList.add('hide');
+  } else {
+    //보임 처리
+    promotionEl.classList.remove('hide')
+  }
+});
